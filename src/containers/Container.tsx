82cff218e178c26714c7d04f2fe7cbff255b6f03
Kevin Cunanan <kevin.cunanan@sureapp.com>
2022-04-07 13:02:46 -0700

    removed comment

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -1,4 +1,3 @@
-/** @jsxImportSource @emotion/react */
 import { useEffect } from 'react';
 import { useLocation, Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
